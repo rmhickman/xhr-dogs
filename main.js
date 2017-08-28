58df87a9a1b8f0dd73d0cdcee3212ce365558d44
Ronnie Hickman <rhickman2305@gmail.com>
2017-08-28 18:48:24 -0500

    console log dog JSON

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("I'm in xhr-dogs");
+function executeThisCodeAfterFileLoads2(){
+	var data = JSON.parse(this.responseText);
+	var dogArray = data.dogs
+	console.log("dogs", dogArray);
+}
+
+function executeThisCodeIfFileErrors2(){
+	console.log("Shit Broke");
+}
+
+var myRequest2 = new XMLHttpRequest();
+myRequest2.addEventListener("load", executeThisCodeAfterFileLoads2);
+myRequest2.addEventListener("error", executeThisCodeIfFileErrors2);
+myRequest2.open("GET", "dogs.json");
+myRequest2.send();
