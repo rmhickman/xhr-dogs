43b2b90afe2d30e47f89c311d067c6eed35afdca
Ronnie Hickman <rhickman2305@gmail.com>
2017-08-28 19:09:46 -0500

    added breeds.json

--- main.js
+++ main.js
@@ -4,7 +4,17 @@ function executeThisCodeAfterFileLoads2(){
 	console.log("dogs", dogArray);
 }
 
+function executeThisCodeAfterFileLoads3(){
+	var data = JSON.parse(this.responseText);
+	var breedsArray = data.breeds
+	console.log("breeds", breedsArray);
+}
+
 function executeThisCodeIfFileErrors2(){
+	console.log("Shit Broke");
+}
+
+function executeThisCodeIfFileErrors3(){
 	console.log("Shit Broke");
 }
 
@@ -13,3 +23,9 @@ myRequest2.addEventListener("load", executeThisCodeAfterFileLoads2);
 myRequest2.addEventListener("error", executeThisCodeIfFileErrors2);
 myRequest2.open("GET", "dogs.json");
 myRequest2.send();
+
+var myRequest3 = new XMLHttpRequest();
+myRequest3.addEventListener("load", executeThisCodeAfterFileLoads3);
+myRequest3.addEventListener("error", executeThisCodeIfFileErrors3);
+myRequest3.open("GET", "breeds.json");
+myRequest3.send();
